eb7e430bc591bf44de02d16e1ffc9db667d55d2c
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2023-04-04 13:49:44 +0500

    add router file

---
+++ router.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import EditEmployee from './features/employee/pages/editemployee'
+import ManageEmployee from './features/employee/pages/manageemployee'
+import ViewEmployee from './features/employee/pages/viewemployee''
+const Dashboard = React.lazy(() => import('./features/dashboard'))
+const Employee = React.lazy(() => import('./features/employee'))
+const Login = React.lazy(() => import('./features/login'))
+
+const RouterLink = () => {
+  return (
+    <>
+      <Routes>
+
+        <Route path='/dashboard'
+          element={
+            <React.Suspense fallback={<>...</>}>
+              <Dashboard/>
+          </React.Suspense>
+        }
+        />
+
+        <Route path='/add-employee'
+          element={
+            <React.Suspense fallback={<>...</>}>
+              <Employee />
+            </React.Suspense>
+          }
+        />
+
+        <Route path='/edit-employee/:employeeId'
+          element={
+            <React.Suspense fallback={<>Loading...</>}>
+              <EditEmployee />
+            </React.Suspense>
+          }
+        />
+
+        <Route path='./manage-employees'
+          element={
+          <React.Suspense>
+            <ManageEmployee/>
+          </React.Suspense>
+          }
+        />
+
+        <Route path='/'
+          element={
+            <React.Suspense fallback={<>...</>}>
+              <Login/>
+            </React.Suspense>
+          }
+        />
+      </Routes>
+    </>
+  )
+}
